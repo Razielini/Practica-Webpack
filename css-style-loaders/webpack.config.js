726ac54c435d701905386a9f6ec4ec59d5ae9757
Razielini <razielco@gmail.com>
2020-05-27 13:30:53 -0600

    CSS Loader

---
+++ css-style-loaders/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, './src/index.js'),
+    jsHome: path.resolve(__dirname, './src/js/index.js'),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
