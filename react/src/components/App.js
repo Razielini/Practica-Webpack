642eae556234f7f3bd4b469b7e0719be390743fb
Razielini <razielco@gmail.com>
2020-05-27 22:07:59 -0600

    Practica con React

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../data/data.json'
+import Loader from './Loader'
 
+console.log(data)
 function App () {
+  const [loaderList, setLoaderList] = useState([])
+
+  const handleClick = () => {
+    setLoaderList(data.loaders)
+  }
+
   return (
     <div>
-      Que linda aplicación
+      <ul>
+        {
+          loaderList.map((item) => (
+            <Loader {...item } key={item.id} />
+          ))
+        }
+      </ul>
+      <button
+        onClick={handleClick}
+      >
+        Mostrar lo aprendido hasta el momento
+      </button>
     </div>
   )
 }
