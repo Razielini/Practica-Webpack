e9f799259288fe9085e17dbb94ca902180dce69a
Razielini <razielco@gmail.com>
2020-05-27 22:28:06 -0600

    Loader Files

---
+++ url-loader/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, './src/index.js')
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 900000
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Plugins',
+      template: path.resolve(__dirname, './src/index.html'),
+    })
+  ]
+}
