035eeb14e763b6cf124ed4b949710aa8d5d7f2b9
Razielini <razielco@gmail.com>
2020-05-28 20:10:22 -0600

    Create index.js

---
+++ prevent-duplication/src/index.js
@@ -0,0 +1,6 @@
+import './styles/styles.css'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+
+render(<App />, document.getElementById('container'))
