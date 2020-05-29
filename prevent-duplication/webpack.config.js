53f97bce6f69c333290f8cdf023a408c0f1abf09
Razielini <razielco@gmail.com>
2020-05-28 20:10:19 -0600

    Create webpack.config.js

---
+++ prevent-duplication/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, './src/index.js'),
+    contact: path.resolve(__dirname, './src/contact.js')
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 900000
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Plugins',
+      template: path.resolve(__dirname, './src/index.html'),
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      name: 'commons',
+      minSize: 0,
+      chunks: 'all'
+    }
+  }
+}
